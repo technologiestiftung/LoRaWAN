5e8a95d2171775257f8c1f9887c1fb91b7350bf5
sebastianrauer <srauer@MAC09.local>
2017-04-25 17:42:17 +0200

    initial commit arduino file

---
+++ sendingecho.ino
@@ -0,0 +1,186 @@
+
+/*******************************************************************************
+ * TSB LoraNode with Ultrasound sensor. 
+ * Based on https://github.com/matthijskooijman/arduino-lmic/blob/master/examples/ttn-abp/ttn-abp.ino
+ * by Thomas Telkamp and Matthijs Kooijman
+ *To use this sketch on another Node please register a new device om TTN and change the Network Session Key, Application Key, etc
+ *******************************************************************************/
+#include <TheThingsNetwork.h>
+#include <NewPing.h>  //Library for ultrasonic sensor
+#include <lmic.h>
+#include <hal/hal.h>
+
+
+/**** Init Ultrasonic******/
+#define TRIGGER_PIN  A0  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     A1  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
+// ***** End Ultrasonic *******/
+
+// Put your LoRaWAN network session key here
+static const PROGMEM u1_t NWKSKEY[16] = { 0x49, 0x4A, 0x96, 0x2B, 0x29, 0x73, 0xBB, 0xB7, 0x1E, 0xC3, 0xDA, 0xE3, 0x0C, 0x16, 0x41, 0xE9 };
+
+// Put your LoRaWAN application session key here
+static const u1_t PROGMEM APPSKEY[16] = { 0x8E, 0x1B, 0xCE, 0x1F, 0xAA, 0x21, 0x1C, 0x45, 0x92, 0x88, 0xFB, 0xC7, 0x39, 0x02, 0x2F, 0xA8 };
+
+// Put your LoRaWAN end-device address (DevAddr) here
+// See http://thethingsnetwork.org/wiki/AddressSpace
+static const u4_t DEVADDR = { 0x26011249 } ; // <-- Change this address for every node!
+
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
+
+static osjob_t sendjob;
+
+int distance;
+static uint8_t message[2];
+
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 60;
+
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+.nss = 10,
+.rxtx = LMIC_UNUSED_PIN,
+.rst = 9,
+.dio = {2, 6, 7}, // Specify pin numbers for DIO0, 1, 2
+// connected to D2, D6, D7
+};
+
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            Serial.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            Serial.println(F("EV_JOINED"));
+            break;
+        case EV_RFU1:
+            Serial.println(F("EV_RFU1"));
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if(LMIC.dataLen) {
+                // data received in rx slot after tx
+                Serial.print(F("Data Received: "));
+                Serial.write(LMIC.frame+LMIC.dataBeg, LMIC.dataLen);
+                Serial.println();
+            }
+            // Schedule next transmission
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            Serial.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            Serial.println(F("EV_RXCOMPLETE"));
+            break;
+        case EV_LINK_DEAD:
+            Serial.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println(F("EV_LINK_ALIVE"));
+            break;
+         default:
+            Serial.println(F("Unknown event"));
+            break;
+    }
+}
+void do_send(osjob_t* j){
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println(F("OP_TXRXPEND, not sending"));
+    } else {
+        // Prepare upstream data transmission at the next possible time.
+        LMIC_setTxData2(1, message, sizeof(message), 0); 
+        Serial.println(F("Packet queued"));
+    }
+    // Next TX is scheduled after TX_COMPLETE event.
+}
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println(F("Starting"));
+    
+  
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+
+    // Set static session parameters. Instead of dynamically establishing a session
+    // by joining the network, precomputed session parameters are be provided.
+    #ifdef PROGMEM
+    // On AVR, these values are stored in flash and only copied to RAM
+    // once. Copy them to a temporary buffer here, LMIC_setSession will
+    // copy them into a buffer of its own again.
+    uint8_t appskey[sizeof(APPSKEY)];
+    uint8_t nwkskey[sizeof(NWKSKEY)];
+    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+    #else
+    // If not running an AVR with PROGMEM, just use the arrays directly 
+    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+    #endif
+
+    // Disable link check validation
+    LMIC_setLinkCheckMode(0);
+
+    // Set data rate and transmit power (note: txpow seems to be ignored by the library)
+    LMIC_setDrTxpow(DR_SF7,14);
+
+}
+
+void loop() {
+      delay(2000);
+  distance = sonar.ping_cm();  //calls ultrasonic sensor via NewPing library
+  
+  if (distance > 10 || distance == 0) { // 0 could be a sensor error, therefore does not trigger
+        Serial.print("Not sending, value: ");
+        Serial.println(distance);
+           
+    } else {
+        Serial.print(F("TRIGGERED! Value: "));
+        Serial.println(distance);
+        message[0] = highByte(distance);
+        message[1] = lowByte(distance);
+        Serial.print(F("Payload value: "));
+        Serial.print(message[0]);
+        Serial.println(message[1]);
+        // Prepare upstream data transmission at the next possible time.
+        do_send(&sendjob);               
+    };
+    os_runloop_once(); // LMIC needs this
+}
